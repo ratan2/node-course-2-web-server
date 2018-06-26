242a6f81b77cecd8fab9cd97ff5e7b8487e043f0
Ratan <rodsgang@gmail.com>
2018-06-26 17:23:51 +0530

    second

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const hbs=require('hbs');
 var app=express();
+const port=process.env.PORT || 3000;
 //hbs.registerPartials(__dirname,'/views/partials');
 app.set('view engine','hbs');
 app.use(express.static(__dirname+'/new'));
@@ -18,5 +19,7 @@ app.get('/about',(req,res)=>{
     res.render('about.hbs',{
       pageTitle:'saa'
     });
-})
-app.listen(3000);
+})  
+app.listen(port,()=>{
+  console.log("server is on");
+});
